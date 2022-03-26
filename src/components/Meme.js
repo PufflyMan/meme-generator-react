2ed3880601e2eea258d12f6055c2c774fd03855b
PufflyMan <ahmedovyusif8785@gmail.com>
2022-03-26 20:05:55 +0400

    update State

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,10 +2,12 @@ import React from "react";
 import memesData from '../data/memesData.js'
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("")
+
     function MemeTrigger() {
         const memesArray = memesData.data.memes
-        const url = memesArray[[(Math.random() * memesArray.length) | 0]].url
-        console.log(url)
+        const rand =(Math.random() * memesArray.length) | 0
+        setMemeImage(memesArray[rand].url)
         
         // const memesArray = memesData.data.memes 
         // const randomNumber = Math.floor(Math.random() * memesArray.length)
@@ -20,6 +22,7 @@ export default function Meme() {
                 <input placeholder="Bottom Text" className="form--input" type="text"></input>
                 <button onClick={MemeTrigger} className="form--button">Get a new meme image 🖼</button>
             </div>
+            <img src={memeImage} className="meme--image"/>
         </main>
     )
 }
